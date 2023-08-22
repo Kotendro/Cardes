c5b34e458610e81e7fa44a0a6d61534dcbd5f82b
Teapare <teapare@outlook.com>
2023-08-22 20:16:00 +0500

    removed redundant format variables from batch template, changed install directory to Program Files

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
-CURRENT_VERSION = "0.1.1"
+CURRENT_VERSION = "0.1.3"
 
-INSTALL_DIR = os.getenv("LOCALAPPDATA") + r"\Programs\Cardes"
+INSTALL_DIR = os.getenv("PROGRAMFILES") + r"\Cardes"
 UPDATE_CACHE_DIR = os.getenv("LOCALAPPDATA") + r"\Cardes\update_cache"
 METADATA_DIR = UPDATE_CACHE_DIR + r"\metadata"
 TARGET_DIR = UPDATE_CACHE_DIR + r"\targets"
@@ -12,11 +12,12 @@ METADATA_BASE_URL = "http://repo.aiop.su:6006/metadata"
 TARGET_BASE_URL = "http://repo.aiop.su:6006/targets"
 
 WIN_BATCH_TEMPLATE = """@echo off
-{log_lines}
+call :log > "{log_file_path}" 2>&1
+:log
 echo Moving app files...
-robocopy "{src_dir}" "{dst_dir}" {robocopy_options}
+robocopy "{src_dir}" "{dst_dir}" /e /move /v /w:2 {purge}
 echo Done.
 start "" "{exe}"
 (goto) 2>nul & del "%~f0"
 """
-LOG_FILE = "update.log"
+LOG_FILE = INSTALL_DIR + "\\update.log"
