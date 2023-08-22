36e94b3c755449c48261d70122d21fed17c99e15
Teapare <teapare@outlook.com>
2023-08-22 20:14:07 +0500

    merged with compress.py

--- root_request.py
+++
@@ -1,5 +0,0 @@
-# Auxiliary script, requests latest root metadata file from the server to pack it with the distribution.
-
-import requests
-with open("root.json", 'wb') as root_file:
-    root_file.write(requests.get(f"http://repo.aiop.su:6006/metadata/{requests.get(f'http://192.168.0.184:6006/').text}.root.json").content)
