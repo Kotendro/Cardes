f726d062be02657ea59413ee3b31fe243cffcaa1
Teapare <teapare@outlook.com>
2023-08-22 20:15:04 +0500

    added logging

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from updater import update
+import logging
+from settings import INSTALL_DIR
 
 if __name__ == '__main__':
+    logger = logging.getLogger()
+    logging.basicConfig(filename=rf"{INSTALL_DIR}\log.txt", filemode='w')
     update()
